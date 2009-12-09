7a35fe47fb94b7d6735354f0208298721c320d1c
Rick Branson <rbranson@rick-bransons-mac-mini.local>
2009-12-09 01:17:55 -0600

    Added benchmark script

---
+++ benchmark.js
@@ -0,0 +1,15 @@
+var FFI = require("./ffi");
+var sys = require("sys");
+var rss = process.memoryUsage()["rss"];
+
+var numberOfMethodsToExecute = 1000000;
+var start = Date.now();
+var absLibrary = new FFI.Library(null, { "abs": [ "int32", [ "int32" ] ] });
+for (var i = 0; i < numberOfMethodsToExecute; i++) {
+    absLibrary.abs(1234);
+}
+var elapsed = (Date.now() - start) / 1000.0;
+sys.puts("Time to execute " + numberOfMethodsToExecute + " functions: " + elapsed + "s (" +
+    numberOfMethodsToExecute / elapsed + " per second)");
+
+sys.puts("Heap increased by " + ((process.memoryUsage()["rss"] - rss) / 1024) + " KB");
