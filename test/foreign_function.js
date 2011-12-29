0e2b688f62a13768658e620622c0ea1af428ad89
Nathan Rajlich <nathan@tootallnate.net>
2011-12-29 13:37:59 -0800

    Add synchronous ForeignFunction test case.

--- test/foreign_function.js
+++ test/foreign_function.js
@@ -4,12 +4,21 @@ var expect = require('expect.js')
 
 describe('ForeignFunction', function () {
 
+  it('should call the static "abs" bindings', function () {
+    var abs = ffi.ForeignFunction.build(
+        ffi.Bindings.StaticFunctions.abs
+      , 'int32', [ 'int32' ])
+    expect(abs).to.be.a('function')
+    expect(abs(-1234)).to.equal(1234)
+  })
+
   describe('async', function () {
 
-    it('should call static "abs" bindings asynchronously', function (done) {
+    it('should call the static "abs" bindings asynchronously', function (done) {
       var abs = ffi.ForeignFunction.build(
                     ffi.Bindings.StaticFunctions.abs
                   , 'int32', [ 'int32' ], true)
+      expect(abs).to.be.a('function')
       abs(-1234).on('success', function (res) {
         expect(res).to.equal(1234)
         done()
