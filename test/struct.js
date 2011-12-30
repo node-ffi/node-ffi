936563aaa599f674fad53ea8b25c755be59322ea
Nathan Rajlich <nathan@tootallnate.net>
2011-12-29 16:28:54 -0800

    Port the struct offsets/sizeof tests to mocha.

--- test/struct.js
+++ test/struct.js
@@ -65,4 +65,124 @@ describe('Struct', function () {
     expect(ms.doubleVal).to.equal(1000.0005)
     expect(ms.pointerVal.address).to.equal(msTestPtr.address)
   })
+
+  describe('offsets and sizeofs', function () {
+
+    function inspect (struct, expectedSize, expectedOffsets) {
+      var info = struct.__structInfo__
+        , props = info.struct
+        , types = []
+
+      Object.keys(props).forEach(function (prop) {
+        var type = props[prop].type
+        if (ffi.isStructType(type)) {
+          // Output more info about the struct
+          type = 'Struct'
+        }
+        types.push(type)
+      })
+
+      it('Struct {'+types.join(', ')+'},  expectedSize='+expectedSize+',  expectedOffsets=['+expectedOffsets.join(',')+']', function () {
+
+        expect(ffi.sizeOf(struct)).to.equal(expectedSize)
+
+        Object.keys(props).forEach(function (prop, i) {
+          expect(props[prop].offset).to.equal(expectedOffsets[i])
+        })
+
+      })
+
+    }
+
+    // TODO: build struct examples statically as part of the build and expose
+    // via the bindings interface.
+
+    var test1 = ffi.Struct([
+        ['int32', 'a']
+      , ['int32', 'b']
+      , ['double', 'c']
+    ])
+    inspect(test1, 16, [0,4,8])
+
+    var test2 = ffi.Struct([
+        ['int32', 'a']
+      , ['double', 'b']
+      , ['int32', 'c']
+    ])
+    inspect(test2, 24, [0,8,16])
+
+    var test3 = ffi.Struct([
+        ['double', 'a']
+      , ['int32', 'b']
+      , ['int32', 'c']
+    ])
+    inspect(test3, 16, [0,8,12])
+
+    var test4 = ffi.Struct([
+        ['double', 'a']
+      , ['double', 'b']
+      , ['int32', 'c']
+    ])
+    inspect(test4, 24, [0,8,16])
+
+    var test5 = ffi.Struct([
+        ['int32', 'a']
+      , ['double', 'b']
+      , ['double', 'c']
+    ])
+    inspect(test5, 24, [0,8,16])
+
+    var test6 = ffi.Struct([
+        ['char', 'a']
+      , ['short','b']
+      , ['int32','c']
+    ])
+    inspect(test6, 8, [0,2,4])
+
+    var test7 = ffi.Struct([
+        ['int32','a']
+      , ['short','b']
+      , ['char', 'c']
+    ])
+    inspect(test7, 8, [0,4,6])
+
+    var test8 = ffi.Struct([
+        ['int32','a']
+      , ['short','b']
+      , ['char', 'c']
+      , ['char', 'd']
+    ])
+    inspect(test8, 8, [0,4,6,7])
+
+    var test9 = ffi.Struct([
+        ['int32','a']
+      , ['short','b']
+      , ['char', 'c']
+      , ['char', 'd']
+      , ['char', 'e']
+    ])
+    inspect(test9, 12, [0,4,6,7,8])
+
+    var test10 = ffi.Struct([
+        [test1, 'a']
+      , ['char','b']
+    ])
+    inspect(test10, 24, [0,16])
+
+    var ffi_type = ffi.Struct([
+        ['size_t','size']
+      , ['ushort','alignment']
+      , ['ushort','type']
+      , ['pointer','elements']
+    ])
+    if (ffi.Bindings.POINTER_SIZE == 4) {
+      inspect(ffi_type, 12, [0,4,6,8])
+    } else if (ffi.Bindings.POINTER_SIZE == 8) {
+      inspect(ffi_type, 24, [0,8,10,16])
+    } else {
+      throw new Error("Bad platform pointer size: %d bytes", ffi.Bindings.POINTER_SIZE)
+    }
+
+  })
+
 })
