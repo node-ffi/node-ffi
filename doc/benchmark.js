1daf088cd23b99f5d621e1639bd3070264924fd0
Nathan Rajlich <nathan@tootallnate.net>
2012-05-05 12:31:16 -0700

    update the benchmark for the new API

--- doc/benchmark.js
+++ doc/benchmark.js
@@ -1,45 +1,46 @@
-var FFI     = require("../lib/ffi"),
-    util    = require("util");
+var ffi = require('../')
+var ref = require('ref')
+var util = require('util')
 
 function measureIterationsOverTime(what, duration, f, granularity) {
-    granularity = granularity || 1000;
+  granularity = granularity || 1000
 
-    var iterations  = 0,
-        start       = Date.now();
+  var iterations  = 0,
+      start       = Date.now()
 
-    while (Date.now() < (start + duration)) {
-        for (var i = 0; i < granularity; i++) {
-            f();
-        }
-        iterations += granularity;
-    };
+  while (Date.now() < (start + duration)) {
+    for (var i = 0; i < granularity; i++) {
+      f()
+    }
+    iterations += granularity
+  }
 
-    var duration    = Date.now() - start,
-        persec      = (iterations / (Date.now() - start)) * 1000;
+  var duration = Date.now() - start
+  var persec   = (iterations / (Date.now() - start)) * 1000
 
-    util.log("Executed " + what + " " + iterations + " times in " + duration + "ms " + "(" + persec + "/sec)");
+  util.log("Executed " + what + " " + iterations + " times in " + duration + "ms " + "(" + persec + "/sec)")
 }
 
 
-var benchLibrary = new FFI.Library(null, {
-    "abs":        [ "int",    [ "int" ] ],
-    "strtoul":    [ "ulong",  [ "string", "pointer", "int" ] ]
-});
+var benchLibrary = new ffi.Library(null, {
+  "abs":        [ "int",    [ "int" ] ],
+  "strtoul":    [ "ulong",  [ "string", "pointer", "int" ] ]
+})
 
-var blabs = benchLibrary.abs;
-var blstrtoul = benchLibrary.strtoul;
+var blabs = benchLibrary.abs
+var blstrtoul = benchLibrary.strtoul
 
-measureIterationsOverTime("FFI abs", 5000, function() {
-    blabs(1234);
-});
+/*measureIterationsOverTime("ffi abs", 5000, function() {
+  blabs(1234)
+})*/
 
-measureIterationsOverTime("FFI strtoul", 5000, function() {
-    blstrtoul("1234567890", null, 0);
-});
+measureIterationsOverTime("ffi strtoul", 5000, function() {
+  blstrtoul("1234567890", null, 0)
+})
 
-strtoulFunc = FFI.Bindings.strtoul;
-strtoulPtr  = FFI.Pointer.NULL;
+strtoulFunc = ffi.Bindings.strtoul
+strtoulPtr  = ref.NULL
 
 measureIterationsOverTime("Binding strtoul", 5000, function() {
-    strtoulFunc("1234567890", strtoulPtr, 0);
-});
+  strtoulFunc("1234567890", strtoulPtr, 0)
+})
