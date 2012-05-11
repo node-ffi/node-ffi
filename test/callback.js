50f3fabf74b6d130dd31da894e89eef37b692692
Nathan Rajlich <nathan@tootallnate.net>
2012-05-11 14:58:11 -0700

    test: put the async callback test into its own namespace

--- test/callback.js
+++ test/callback.js
@@ -1,6 +1,8 @@
 
 var assert = require('assert')
+  , ref = require('ref')
   , ffi = require('../')
+  , int = ref.types.int
 
 describe('Callback', function () {
 
@@ -12,18 +14,22 @@ describe('Callback', function () {
   })
 
   it('should be invokable by an ffi\'d ForeignFunction', function () {
-    var funcPtr = ffi.Callback('int', [ 'int' ], Math.abs)
-    var func = ffi.ForeignFunction(funcPtr, 'int', [ 'int' ])
+    var funcPtr = ffi.Callback(int, [ int ], Math.abs)
+    var func = ffi.ForeignFunction(funcPtr, int, [ int ])
     assert.equal(1234, func(-1234))
   })
 
-  it('should be invokable asynchronously by an ffi\'d ForeignFunction', function () {
-    var funcPtr = ffi.Callback('int', [ 'int' ], Math.abs)
-    var func = ffi.ForeignFunction(funcPtr, 'int', [ 'int' ])
-    func.async(-9999, function (err, res) {
-      assert.equal(null, err)
-      assert.equal(9999, res)
+  describe('async', function () {
+
+    it('should be invokable asynchronously by an ffi\'d ForeignFunction', function () {
+      var funcPtr = ffi.Callback(int, [ int ], Math.abs)
+      var func = ffi.ForeignFunction(funcPtr, int, [ int ])
+      func.async(-9999, function (err, res) {
+        assert.equal(null, err)
+        assert.equal(9999, res)
+      })
     })
+
   })
 
 })
