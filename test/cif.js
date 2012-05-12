0e56eaf86084f1e8460a70300099e850ca560388
Nathan Rajlich <nathan@tootallnate.net>
2012-05-12 10:58:33 -0700

    test: add a test case for an FFI_BAD_TYPEDEF cif() error

--- test/cif.js
+++ test/cif.js
@@ -12,6 +12,24 @@ describe('ffi_cif', function () {
     assert(Buffer.isBuffer(cif))
   })
 
+  it('should throw an Error when given an invalid "type"', function () {
+    var ffi_type = new ffi.FFI_TYPE
+    ffi_type.size = 0
+    ffi_type.alignment = 0
+    ffi_type.type = 0
+    ffi_type.elements = ref.NULL
+
+    var bad_type = { size: 1, indirection: 1, ffi_type: ffi_type.ref() }
+    assert.throws(function () {
+      ffi.CIF(bad_type, [])
+    }, function (err) {
+      assert(err.message.indexOf('FFI_BAD_TYPEDEF') !== -1)
+      assert.equal('FFI_BAD_TYPEDEF', err.code)
+      assert.equal(ffi.Bindings.FFI_BAD_TYPEDEF, err.errno)
+      return true
+    })
+  })
+
   it('should throw an Error when given an invalid ABI argument', function () {
     assert.throws(function () {
       ffi.CIF(ref.types.void, [], -1)
