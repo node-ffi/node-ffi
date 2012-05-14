42a12cbb59d0dd3c78b08e8be986e5ba23048f07
Nathan Rajlich <nathan@tootallnate.net>
2012-05-14 10:46:16 -0700

    a little refactoring of factorial example

--- example/factorial/factorial.c
+++ example/factorial/factorial.c
@@ -1,11 +1,12 @@
 #include <stdint.h>
 
 uint64_t factorial(int max) {
-    int i           = max;
-    uint64_t result = 1;
+  int i = max;
+  uint64_t result = 1;
 
-    while (i >= 2)
-        result *= i--;
+  while (i >= 2) {
+    result *= i--;
+  }
 
-    return result;
+  return result;
 }
--- example/factorial/factorial.js
+++ example/factorial/factorial.js
@@ -1,12 +1,12 @@
-var ffi = require('../../lib/ffi')
+var ffi = require('../../')
 
 var libfactorial = ffi.Library('./libfactorial', {
-    'factorial': [ 'uint64', [ 'int' ] ]
+  'factorial': [ 'uint64', [ 'int' ] ]
 })
 
 if (process.argv.length < 3) {
-    console.log('Arguments: ' + process.argv[0] + ' ' + process.argv[1] + ' <max>')
-    process.exit()
+  console.log('Arguments: ' + process.argv[0] + ' ' + process.argv[1] + ' <max>')
+  process.exit()
 }
 
 var output = libfactorial.factorial(parseInt(process.argv[2]))
