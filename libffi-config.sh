6560822ee17e447c81efe9ca1ac78b60f36b7111
Jairo Luiz <jairo.luiz@tangerinagames.com>
2012-05-24 12:12:08 -0400

    fixed the compile problem with x64 linux

--- libffi-config.sh
+++ libffi-config.sh
@@ -1,4 +1,9 @@
+
+if [ `getconf LONG_BIT` -eq 64 ]; then
+  CFLAGS="-fPIC"
+fi
+
 cd deps/libffi
 make clean distclean >node_ffi_configure.out 2>&1
-sh configure --enable-static --disable-shared --disable-builddir >>node_ffi_configure.out 2>&1
+sh configure --enable-static --disable-shared --disable-builddir CFLAGS="$CFLAGS" >>node_ffi_configure.out 2>&1
 cd ../..
