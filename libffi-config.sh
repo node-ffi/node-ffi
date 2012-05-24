18954cbc02cb3061312b9c75cb362c9753840f18
Jairo Luiz <jairo.luiz@tangerinagames.com>
2012-05-24 12:20:07 -0400

    remove junk

--- libffi-config.sh
+++ libffi-config.sh
@@ -1,8 +1,3 @@
-
-if [ `getconf LONG_BIT` -eq 64 ]; then
-  CFLAGS="-fPIC"
-fi
-
 cd deps/libffi
 make clean distclean >node_ffi_configure.out 2>&1
 sh configure --enable-static --disable-shared --disable-builddir --with-pic >>node_ffi_configure.out 2>&1
