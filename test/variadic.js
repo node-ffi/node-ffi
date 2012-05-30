2c06bc1cd6d7f2a705cf75cc2a6b9ffbdeadd4ed
Nathan Rajlich <nathan@tootallnate.net>
2012-05-29 20:34:24 -0700

    test: use `snprintf()` for the variadic test

--- test/variadic.js
+++ test/variadic.js
@@ -8,9 +8,18 @@ describe('varargs', function () {
   afterEach(gc)
 
   it('should work with vararg C functions', function () {
-    var printfPtr = ffi.DynamicLibrary().get('printf')
-    var printfGen = ffi.VariadicForeignFunction(printfPtr, 'int', [ 'string' ])
-    printfGen()('hello world!')
+    var buf = new Buffer(100)
+    var snprintfPtr = ffi.DynamicLibrary().get('snprintf')
+    var snprintfGen = ffi.VariadicForeignFunction(snprintfPtr, 'int', [ 'pointer', 'size_t', 'string' ])
+
+    snprintfGen()(buf, buf.length, 'hello world!')
+    assert.equal(buf.readCString(), 'hello world!')
+
+    snprintfGen('int')(buf, buf.length, '%d', 42)
+    assert.equal(buf.readCString(), '42')
+
+    snprintfGen('double')(buf, buf.length, '%10.2f', 3.14)
+    assert.equal(buf.readCString(), '      3.14')
   })
 
 })
