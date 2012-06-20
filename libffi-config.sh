d198c31495e77d6e777f05526c6404ab97635480
Nathan Rajlich <nathan@tootallnate.net>
2012-06-20 11:56:15 -0700

    build: first attempt at getting the windows build working again

--- libffi-config.sh
+++ libffi-config.sh
@@ -1,4 +1,10 @@
 cd deps/libffi
+if [[ "$OSTYPE" == "msys" ]]; then
+  export CC="`pwd`/msvcc.sh"
+  export LD="link"
+  export CPP="cl -nologo -EP"
+  export CFLAGS=""
+fi
 make clean distclean >node_ffi_configure.out 2>&1
 sh configure --enable-static --disable-shared --disable-builddir --with-pic >>node_ffi_configure.out 2>&1
 cd ../..
