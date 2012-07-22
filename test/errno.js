f499f7925bc6c9f7ff57f6d23d53149aa172d482
Nathan Rajlich <nathan@tootallnate.net>
2012-07-21 18:47:00 -0700

    test: use "string" instead of "char *"

--- test/errno.js
+++ test/errno.js
@@ -3,7 +3,6 @@ var assert = require('assert')
   , ref = require('ref')
   , ffi = require('../')
   , errno = ffi.errno
-  , charPtr = ref.refType(ref.types.char)
 
 describe('errno()', function () {
 
@@ -16,7 +15,7 @@ describe('errno()', function () {
   it('should set the errno with out-of-range "strtoul" value', function () {
     var lib = process.platform == 'win32' ? 'msvcrt' : 'libc'
     var strtoul = new ffi.Library(lib, {
-      'strtoul': [ 'ulong', [ 'string', charPtr, 'int' ] ]
+      'strtoul': [ 'ulong', [ 'string', 'string', 'int' ] ]
     }).strtoul
     var before = errno()
     strtoul('1234567890123456789012345678901234567890', null, 0)
