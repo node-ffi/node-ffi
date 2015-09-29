f5e445be913484cf141fd1752f77c4c77c8b5f72
Nathan Rajlich <nathan@tootallnate.net>
2015-09-29 14:20:28 -0700

    test: load Foundation first instead
    It's more low-level than Cocoa, and contains NSException directly.

--- test/objc.js
+++ test/objc.js
@@ -14,10 +14,10 @@ var Class = voidPtr;
 if (ffi.HAS_OBJC) {
 
   describe('@try / @catch', function () {
-    // not entirely sure why this works, but we have to load `Cocoa` first,
+    // not entirely sure why this works, but we have to load `Foundation` first,
     // otherwise Objective-C exceptions will not work. Magic!
     // https://github.com/node-ffi/node-ffi/issues/195
-    var lib = ffi.DynamicLibrary('/System/Library/Frameworks/Cocoa.framework/Versions/A/Cocoa');
+    var lib = ffi.DynamicLibrary('/System/Library/Frameworks/Foundation.framework/Versions/Current/Foundation')
 
     afterEach(gc);
 
