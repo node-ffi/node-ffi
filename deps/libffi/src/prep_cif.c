2cf0a3ff260005c45fd58b3398cbc16085eadee4
Nikolai Ommundsen <post@niikoo.net>
2018-09-21 00:18:40 +0200

    Builds on Ubuntu 18.04 x64

--- deps/libffi/src/prep_cif.c
+++ deps/libffi/src/prep_cif.c
@@ -50,22 +50,20 @@ static ffi_status initialize_aggregate(ffi_type *arg)
     return FFI_BAD_TYPEDEF;
 
   while ((*ptr) != NULL)
-    {
-      if (UNLIKELY(((*ptr)->size == 0)
-		    && (initialize_aggregate((*ptr)) != FFI_OK)))
-	return FFI_BAD_TYPEDEF;
+  {
+    if (UNLIKELY(((*ptr)->size == 0) && (initialize_aggregate((*ptr)) != FFI_OK)))
+      return FFI_BAD_TYPEDEF;
 
-      /* Perform a sanity check on the argument type */
-      FFI_ASSERT_VALID_TYPE(*ptr);
+    /* Perform a sanity check on the argument type */
+    FFI_ASSERT_VALID_TYPE(*ptr);
 
-      arg->size = ALIGN(arg->size, (*ptr)->alignment);
-      arg->size += (*ptr)->size;
+    arg->size = ALIGN(arg->size, (*ptr)->alignment);
+    arg->size += (*ptr)->size;
 
-      arg->alignment = (arg->alignment > (*ptr)->alignment) ?
-	arg->alignment : (*ptr)->alignment;
+    arg->alignment = (arg->alignment > (*ptr)->alignment) ? arg->alignment : (*ptr)->alignment;
 
-      ptr++;
-    }
+    ptr++;
+  }
 
   /* Structure size includes tail padding.  This is important for
      structures that fit in one register on ABIs like the PowerPC64
@@ -74,7 +72,7 @@ static ffi_status initialize_aggregate(ffi_type *arg)
      struct A { long a; char b; }; struct B { struct A x; char y; };
      should find y at an offset of 2*sizeof(long) and result in a
      total size of 3*sizeof(long).  */
-  arg->size = ALIGN (arg->size, arg->alignment);
+  arg->size = ALIGN(arg->size, arg->alignment);
 
   /* On some targets, the ABI defines that structures have an additional
      alignment beyond the "natural" one based on their elements.  */
@@ -103,12 +101,11 @@ static ffi_status initialize_aggregate(ffi_type *arg)
    For variadic calls, isvariadic should be 1 and nfixedargs
    and ntotalargs set as appropriate. nfixedargs must always be >=1 */
 
-
 ffi_status FFI_HIDDEN ffi_prep_cif_core(ffi_cif *cif, ffi_abi abi,
-			     unsigned int isvariadic,
-                             unsigned int nfixedargs,
-                             unsigned int ntotalargs,
-			     ffi_type *rtype, ffi_type **atypes)
+                                        unsigned int isvariadic,
+                                        unsigned int nfixedargs,
+                                        unsigned int ntotalargs,
+                                        ffi_type *rtype, ffi_type **atypes)
 {
   unsigned bytes = 0;
   unsigned int i;
@@ -118,7 +115,7 @@ ffi_status FFI_HIDDEN ffi_prep_cif_core(ffi_cif *cif, ffi_abi abi,
   FFI_ASSERT((!isvariadic) || (nfixedargs >= 1));
   FFI_ASSERT(nfixedargs <= ntotalargs);
 
-  if (! (abi > FFI_FIRST_ABI && abi < FFI_LAST_ABI))
+  if (!(abi > FFI_FIRST_ABI && abi < FFI_LAST_ABI))
     return FFI_BAD_ABI;
 
   cif->abi = abi;
@@ -129,7 +126,7 @@ ffi_status FFI_HIDDEN ffi_prep_cif_core(ffi_cif *cif, ffi_abi abi,
   cif->flags = 0;
 
 #if HAVE_LONG_DOUBLE_VARIANT
-  ffi_prep_types (abi);
+  ffi_prep_types(abi);
 #endif
 
   /* Initialize the return type if necessary */
@@ -159,64 +156,61 @@ ffi_status FFI_HIDDEN ffi_prep_cif_core(ffi_cif *cif, ffi_abi abi,
 #ifdef NIOS2
       && (cif->rtype->size > 8)
 #endif
-     )
-    bytes = STACK_ARG_SIZE(sizeof(void*));
+  )
+    bytes = STACK_ARG_SIZE(sizeof(void *));
 #endif
 
   for (ptr = cif->arg_types, i = cif->nargs; i > 0; i--, ptr++)
-    {
+  {
 
-      /* Initialize any uninitialized aggregate type definitions */
-      if (((*ptr)->size == 0) && (initialize_aggregate((*ptr)) != FFI_OK))
-	return FFI_BAD_TYPEDEF;
+    /* Initialize any uninitialized aggregate type definitions */
+    if (((*ptr)->size == 0) && (initialize_aggregate((*ptr)) != FFI_OK))
+      return FFI_BAD_TYPEDEF;
 
 #ifndef FFI_TARGET_HAS_COMPLEX_TYPE
-      if ((*ptr)->type == FFI_TYPE_COMPLEX)
-	abort();
+    if ((*ptr)->type == FFI_TYPE_COMPLEX)
+      abort();
 #endif
-      /* Perform a sanity check on the argument type, do this
+    /* Perform a sanity check on the argument type, do this
 	 check after the initialization.  */
-      FFI_ASSERT_VALID_TYPE(*ptr);
+    FFI_ASSERT_VALID_TYPE(*ptr);
 
 #if !defined FFI_TARGET_SPECIFIC_STACK_SPACE_ALLOCATION
 #ifdef SPARC
-      if (((*ptr)->type == FFI_TYPE_STRUCT
-	   && ((*ptr)->size > 16 || cif->abi != FFI_V9))
-	  || ((*ptr)->type == FFI_TYPE_LONGDOUBLE
-	      && cif->abi != FFI_V9))
-	bytes += sizeof(void*);
-      else
+    if (((*ptr)->type == FFI_TYPE_STRUCT && ((*ptr)->size > 16 || cif->abi != FFI_V9)) || ((*ptr)->type == FFI_TYPE_LONGDOUBLE && cif->abi != FFI_V9))
+      bytes += sizeof(void *);
+    else
 #endif
-	{
-	  /* Add any padding if necessary */
-	  if (((*ptr)->alignment - 1) & bytes)
-	    bytes = (unsigned)ALIGN(bytes, (*ptr)->alignment);
+    {
+      /* Add any padding if necessary */
+      if (((*ptr)->alignment - 1) & bytes)
+        bytes = (unsigned)ALIGN(bytes, (*ptr)->alignment);
 
 #ifdef TILE
-	  if (bytes < 10 * FFI_SIZEOF_ARG &&
-	      bytes + STACK_ARG_SIZE((*ptr)->size) > 10 * FFI_SIZEOF_ARG)
-	    {
-	      /* An argument is never split between the 10 parameter
+      if (bytes < 10 * FFI_SIZEOF_ARG &&
+          bytes + STACK_ARG_SIZE((*ptr)->size) > 10 * FFI_SIZEOF_ARG)
+      {
+        /* An argument is never split between the 10 parameter
 		 registers and the stack.  */
-	      bytes = 10 * FFI_SIZEOF_ARG;
-	    }
+        bytes = 10 * FFI_SIZEOF_ARG;
+      }
 #endif
 #ifdef XTENSA
-	  if (bytes <= 6*4 && bytes + STACK_ARG_SIZE((*ptr)->size) > 6*4)
-	    bytes = 6*4;
+      if (bytes <= 6 * 4 && bytes + STACK_ARG_SIZE((*ptr)->size) > 6 * 4)
+        bytes = 6 * 4;
 #endif
 
-	  bytes += STACK_ARG_SIZE((*ptr)->size);
-	}
-#endif
+      bytes += STACK_ARG_SIZE((*ptr)->size);
     }
+#endif
+  }
 
   cif->bytes = bytes;
 
   /* Perform machine dependent cif processing */
 #ifdef FFI_TARGET_SPECIFIC_VARIADIC
   if (isvariadic)
-	return ffi_prep_cif_machdep_var(cif, nfixedargs, ntotalargs);
+    return ffi_prep_cif_machdep_var(cif, nfixedargs, ntotalargs);
 #endif
 
   return ffi_prep_cif_machdep(cif);
@@ -224,7 +218,7 @@ ffi_status FFI_HIDDEN ffi_prep_cif_core(ffi_cif *cif, ffi_abi abi,
 #endif /* not __CRIS__ */
 
 ffi_status ffi_prep_cif(ffi_cif *cif, ffi_abi abi, unsigned int nargs,
-			     ffi_type *rtype, ffi_type **atypes)
+                        ffi_type *rtype, ffi_type **atypes)
 {
   return ffi_prep_cif_core(cif, abi, 0, nargs, nargs, rtype, atypes);
 }
@@ -242,12 +236,12 @@ ffi_status ffi_prep_cif_var(ffi_cif *cif,
 #if FFI_CLOSURES
 
 ffi_status
-ffi_prep_closure (ffi_closure* closure,
-		  ffi_cif* cif,
-		  void (*fun)(ffi_cif*,void*,void**,void*),
-		  void *user_data)
+ffi_prep_closure(ffi_closure *closure,
+                 ffi_cif *cif,
+                 void (*fun)(ffi_cif *, void *, void **, void *),
+                 void *user_data)
 {
-  return ffi_prep_closure_loc (closure, cif, fun, user_data, closure);
+  return ffi_prep_closure_loc(closure, cif, fun, user_data, closure);
 }
 
 #endif
